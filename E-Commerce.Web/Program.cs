4388ec93dcfde7390398425e3ceca1deecef9e1e
Dina Magdy <dhammad339@gmail.com>
2025-04-20 12:32:13 +0200

    06- Products - Brands - Types Data Seeding

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,5 +1,7 @@
 
+using DomainLayer.Contracts;
 using Microsoft.EntityFrameworkCore;
+using Persistence;
 using Persistence.Data;
 
 namespace E_Commerce.Web
@@ -10,21 +12,27 @@ namespace E_Commerce.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            #region Add services to the container  
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle  
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddDbContext<StoreDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
-
+            builder.Services.AddScoped<IDataSeeding, DataSeeding>();
+            #endregion
 
             var app = builder.Build();
+            using (var scope = app.Services.CreateScope())
+            {
+                var ObjectOfDataSeeding = scope.ServiceProvider.GetRequiredService<IDataSeeding>();
+                ObjectOfDataSeeding.DataSeed();
+            }
 
-            // Configure the HTTP request pipeline.
+            #region Configure the HTTP request pipeline  
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -35,8 +43,8 @@ namespace E_Commerce.Web
 
             app.UseAuthorization();
 
-
             app.MapControllers();
+            #endregion
 
             app.Run();
         }
