de868157d23290590d3107276780a6df28189e4c
Dina Magdy <dhammad339@gmail.com>
2025-04-21 14:34:22 +0200

    08- Product Controller

--- Infrastructure/Presentation/Controllers/ProductsController.cs
+++ Infrastructure/Presentation/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using ServiceAbstraction;
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransferObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +9,43 @@ using System.Threading.Tasks;
 
 namespace Presentation.Controllers
 {
-    public class ProductsController (IServiceManager serviceManager)
+    [ApiController]
+    [Route("api/[controller]")] // BaseUrl/api/products
+    public class ProductsController (IServiceManager _serviceManager) : ControllerBase
     {
+        // Get All Products
+        // GET: BaseUrl/api/products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTo>>> GetAllProducts() 
+        {
+            var products = await _serviceManager.ProductService.GetAllProductsAsync();
+            return Ok(products);
+        }
+        // Get Product By Id
+        // GET: BaseUrl/api/products/{id}
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductDTo> >GetProductById(int id)
+        {
+            var product = await _serviceManager.ProductService.GetProductByIdAsync(id);
+            return Ok(product);
+        }
+        // Get All Brands
+        // GET: BaseUrl/api/products/brands
+        [HttpGet("brands")]
+        public async Task<ActionResult<IEnumerable<BrandDTo>>> GetAllBrands()
+        {
+            var brands = await _serviceManager.ProductService.GetAllBrandsAsync();
+            return Ok(brands);
+        }
 
-   
+        // Get All Types
+        // GET: BaseUrl/api/products/types
+        [HttpGet("types")]
+        public async Task<ActionResult<IEnumerable<TypeDTo>>> GetAllTypes()
+        {
+            var types = await _serviceManager.ProductService.GetAllTypessAsync();
+            return Ok(types);
+        }
+
     }
 }
