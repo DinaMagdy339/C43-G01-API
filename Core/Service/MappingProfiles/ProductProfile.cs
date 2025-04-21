af71694d084b8bf165c17e0448c8624e8a9fe676
Dina Magdy <dhammad339@gmail.com>
2025-04-21 12:31:56 +0200

    Edit Auto Mapper

--- Core/Service/MappingProfiles/ProductProfile.cs
+++ Core/Service/MappingProfiles/ProductProfile.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using DomainLayer.Models;
+using Shared.DataTransferObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,15 @@ using System.Threading.Tasks;
 
 namespace Service.MappingProfiles
 {
-    internal class ProductProfile
+    public class ProductProfile : Profile
     {
+        public ProductProfile() 
+        {
+            CreateMap<Product, ProductDTo>()
+                .ForMember(dest => dest.BrandName, opt => opt.MapFrom(src => src.ProductBrand.Name))
+                .ForMember(dest => dest.TypeName, opt => opt.MapFrom(src => src.ProductType.Name));
+            CreateMap<ProductBrand, BrandDTo>();
+            CreateMap<ProductType, TypeDTo>();
+        }
     }
 }
